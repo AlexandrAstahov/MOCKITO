add794a038b55a9d903c7fb7e4d71e259e432076
AleksandrAstahov <spirtt1985@yandex.ru>
2022-06-29 18:18:19 +0300

    Add FilmManager.java

---
+++ src/main/java/ru/netology/FilmManager.java
@@ -0,0 +1,51 @@
+package ru.netology;
+
+public class FilmManager {
+    private String[] movies = new String[0];
+    private int limit;
+
+    public FilmManager(){
+        limit = 10;
+    }
+
+    public FilmManager (int limit){
+        this.limit = limit;
+    }
+
+
+    // Добавление нового фильма
+    public void addMovie (String movie){
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    //Вывод всех фильмов в порядке добавления
+    public String[] findAll (){
+        return movies;
+    }
+
+    //Вывод последних добавленных фильмов
+    public String[] findLast(){
+        String[] last;
+        if (limit < movies.length){
+            last = new String[limit];
+        } else {
+            last = new String[movies.length];
+        }
+        for (int i = 0; i < last.length; i++) {
+            last[i] = movies[movies.length - 1];
+        }
+        return last;
+
+    }
+
+
+
+
+
+
+}
